d221ffec088e9e4ff5bca87f1b20948a34f08116
Navnath <navnathashruba.andhale@cognizant.com>
2020-09-27 01:02:55 +0530

    Removed Google API key.

--- app/src/main/java/com/johnsoncontrol/nearbyplacesfinderapp/utils/Constants.kt
+++ app/src/main/java/com/johnsoncontrol/nearbyplacesfinderapp/utils/Constants.kt
@@ -2,7 +2,7 @@ package com.johnsoncontrol.nearbyplacesfinderapp.utils
 
 object Constants {
     const val ALL_PLACE_TYPE = "restaurant|hospitals|pubs|schools"
-    const val GOOGLE_PLACE_API_KEY = "AIzaSyCeDOeUcpLmw56dnVS6-25PaLlWNvxshNM"
+    const val GOOGLE_PLACE_API_KEY = "Place your google api key"
     const val RADIUS_5K = "5000"
     const val RADIUS_10K = "10000"
     const val RADIUS_15K = "15000"
